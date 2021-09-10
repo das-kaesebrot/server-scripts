87ee35a449bd946002d6497e0f44cab8598254a4
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-09-10 02:36:37 +0200

    add: More verbose output for Wireguard script

--- wireguard/add-peer-interactively.sh
+++ wireguard/add-peer-interactively.sh
@@ -9,6 +9,8 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 BOLD='\033[1m'
 
+OKP="[  ${GREEN}OK${NC}  ]"
+
 
 # check if user is root
 if (( $EUID != 0 )); then
@@ -32,7 +34,7 @@ do
 	done
 	read -p "Wireguard interface to modify? " IFACENUM
 done
-echo -e "[  ${GREEN}OK${NC}  ] Selected interface: ${BOLD}${WGINTERFACES[$IFACENUM]}${NC}"
+echo -e "$OKP Selected interface: ${BOLD}${WGINTERFACES[$IFACENUM]}${NC}"
 IFACE=${WGINTERFACES[$IFACENUM]}
 
 # Read in endpoint port and address
@@ -44,7 +46,7 @@ do
 	POSSIBLEENDPOINTS+=($(ip -o -6 addr list ${NETDEVS[$i]} | awk '{print $4}' | cut -f1 -d'/'))
 done
 
-ENDPOINTPORT=$(wg show ${WGINTERFACES[$IFACENUM]} listen-port)
+ENDPOINTPORT=$(wg show $IFACE listen-port)
 
 echo "Possible endpoint values:"
 for (( i=0; i<${#POSSIBLEENDPOINTS[@]}; i++ ))
@@ -64,23 +66,23 @@ fi
 read -p "Endpoint port? [$ENDPOINTPORT]: " ENDPOINTPORTTMP
 ENDPOINTPORT=${ENDPOINTPORTTMP:-$ENDPOINTPORT}
 ENDPOINT="$ENDPOINT:$ENDPOINTPORT"
-echo -e "[  ${GREEN}OK${NC}  ] Endpoint set to: ${BOLD}$ENDPOINT${NC}"
+echo -e "$OKP Endpoint set to: ${BOLD}$ENDPOINT${NC}"
 
 read -p "Peer name? " PEERNAME
-echo -e "[  ${GREEN}OK${NC}  ] Peer file will be written to: ${BOLD}peers/$PEERNAME.conf${NC}"
+echo -e "$OKP Peer file will be written to: ${BOLD}peers/$PEERNAME.conf${NC}"
 
-# get highest ip of all current peers and increment by 1
+# get highest ip of all current peers and increment by 1 (assumes that only the last octet is used)
 IP=$(wg show $IFACE allowed-ips | awk '{print $2}' | rev | cut -c4- | rev | cut -f4- -d'.' | sort -r | awk 'NR==1')
 ((IP++))
-# get subnet for client uo
+# get subnet for client ip
 SUBNET=$(wg show $IFACE allowed-ips | awk '{print $2}' | rev | cut -c4- | cut -f2- -d'.' | rev | awk 'NR==1')
 CLIENTIP="$SUBNET.$IP/32"
 
 read -p "Client IP? [$CLIENTIP]: " CLIENTIPTMP
 read -p "AllowedIPs (client)? " ALLOWEDIPS
 CLIENTIP=${CLIENTIPTMP:-$CLIENTIP}
-echo -e "[  ${GREEN}OK${NC}  ] Client IP set to: ${BOLD}$CLIENTIP${NC}"
-echo -e "[  ${GREEN}OK${NC}  ] AllowedIPs set to: ${BOLD}$ALLOWEDIPS${NC}"
+echo -e "$OKP Client IP set to: ${BOLD}$CLIENTIP${NC}"
+echo -e "$OKP AllowedIPs set to: ${BOLD}$ALLOWEDIPS${NC}"
 
 # generate the keys
 wg genkey > privatekey.tmp
@@ -88,7 +90,9 @@ cat privatekey.tmp | wg pubkey > pubkey.tmp
 
 # read keys into variables
 PRIVATEKEY=$(cat privatekey.tmp)
+echo -e "$OKP Generated peer's private key"
 PUBLICKEY=$(cat pubkey.tmp)
+echo -e "$OKP Generated peer's public key ${BOLD}$PUBLICKEY${NC}"
 SERVER_PUBLICKEY=$(wg show $IFACE public-key)
 
 # Generate client and server configs
@@ -112,9 +116,14 @@ PersistentKeepalive = 25
 # Write configs to server or client files
 mkdir -p "peers"
 echo "$CONF_CLIENT" > "peers/$PEERNAME.conf"
+echo -e "$OKP Wrote client file to ${BOLD}peers/$PEERNAME.conf${NC}"
 echo "$CONF_SERVER" >> "/etc/wireguard/$IFACE.conf"
+echo -e "$OKP Appended new client to $IFACE config at ${BOLD}/etc/wireguard/$IFACE.conf${NC}"
 
 wg setconf $IFACE <(wg-quick strip $IFACE)
+echo -e "$OKP Reloaded Wireguard interface ${BOLD}$IFACE${NC}"
 
 # remove tmp files
 rm privatekey.tmp pubkey.tmp
+echo -e "$OKP Removed temporary key files"
+echo -e "Done!"
